2f4301cf116dbe4e12973496f7c7026e0d9b9fa2
Larissa <larissabmac@gmail.com>
2025-06-03 20:45:08 -0300

    Organizando de acordo com a estrutura definida

--- back/src/modules/login/routes/user-routes.ts
+++ back/src/modules/login/routes/user-routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register, login } from '../../../../user.controller';
+import { register, login } from '../persistence/user-controller.ts';
 
 const router = express.Router();
 
