25dffd09a42511dd113ad56b6dee47d3593099c3
Jeielma <jeielma66476@unieuro.com.br>
2025-06-04 21:09:46 -0300

    Adicionar tabela equipamento e tipoinstalacao

---
+++ back/src/modules/laudo/persistence/Equipamento-persistence.ts
@@ -0,0 +1,69 @@
+import { PrismaClient } from "@prisma/client";
+
+export class equipamentoPersistence {
+  private prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = new PrismaClient();
+  }
+
+  async create(equipamentoData: { equipamentonome: string; equipamentostatus?: number }) {
+    try {
+      const equipamento = await this.prisma.equipamento.create({
+        data: equipamentoData,
+      });
+      return equipamento;
+    } catch (error) {
+      console.error("Error creating equipamento:", error);
+      throw error;
+    }
+  }
+
+  async findAll() {
+    try {
+      const equipamentos = await this.prisma.equipamento.findMany();
+      return equipamentos;
+    } catch (error) {
+      console.error("Error fetching equipamentos:", error);
+      throw error;
+    }
+  }
+
+  async findById(id: bigint) {
+    try {
+      const equipamento = await this.prisma.equipamento.findUnique({
+        where: { idequipamento: id },
+      });
+      return equipamento;
+    } catch (error) {
+      console.error("Error fetching equipamento by ID:", error);
+      throw error;
+    }
+  }
+
+  async update(id: bigint, equipamentoData: { equipamentonome?: string; equipamentostatus?: number }) {
+    try {
+      const updatedEquipamento = await this.prisma.equipamento.update({
+        where: { idequipamento: id },
+        data: equipamentoData,
+      });
+      return updatedEquipamento;
+    } catch (error) {
+      console.error("Error updating equipamento:", error);
+      throw error;
+    }
+  }
+
+  async delete(id: bigint) {
+    try {
+      const deletedEquipamento = await this.prisma.equipamento.delete({
+        where: { idequipamento: id },
+      });
+      return deletedEquipamento;
+    } catch (error) {
+      console.error("Error deleting equipamento:", error);
+      throw error;
+    }
+  }
+}
+
