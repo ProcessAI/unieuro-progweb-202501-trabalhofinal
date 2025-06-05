372452ba82ff4fcdd64721201f23b5ef8d3f01ae
Paulo Costa <paulohspc@gmail.com>
2025-06-05 20:40:24 -0300

    front com validadores

---
+++ back/src/modules/laudo/service/tipoeqservice.ts
@@ -0,0 +1,24 @@
+export async function create(tipoeqData:any) {
+//TODO
+return
+}
+
+export async function findAll() {
+//TODO
+return
+}
+
+export async function findById(id: number) {
+//TODO
+return
+}
+
+export async function update(id: number, tipoeqdata:any) {
+//TODO
+return
+}
+
+export async function deletetipoeq(id: number) {
+//TODO
+return
+}
--- front/src/pages/TipoeqCrud.tsx
+++ front/src/pages/TipoeqCrud.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
+import {useForm}  from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { tipoEquipamentoSchema } from '../hooks/tipoEquipamentoSchema'; // ajuste conforme seu projeto
 import './TipoeqCrud.css';
