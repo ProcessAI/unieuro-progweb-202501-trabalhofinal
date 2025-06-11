7ff5173a7c691d643e2a3765ba16adcd550412ba
Larissa <larissabmac@gmail.com>
2025-06-10 22:15:22 -0300

    GetAllSedes e CreateSede funcionando, além da documentação desses endpoints

--- back/src/modules/router/app.ts
+++ back/src/modules/router/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import userRoutes from '../login/routes/usuario-routes';
 import protegidoRoutes from '../login/routes/auth-middleware-routes';
+import sedeRoutes from '../laudo/routes/sede-routes';
 
 
 const app = express();
 
 app.use(express.json());
 app.use('/auth', userRoutes);
-app.use('/api', protegidoRoutes); // ou o path que quiser
+app.use('/api', protegidoRoutes);
+app.use('/sede', sedeRoutes);
 
 export default app;
