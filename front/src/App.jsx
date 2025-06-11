a0bf662455cc38b02805009ef753af4ab5e6c0b7
Ezio <67691964+EzioPP@users.noreply.github.com>
2025-06-11 19:18:23 -0300

    Adicione STUB para componente Markdown

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,18 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Login from "./pages/Login";
-import Cadastro from "./pages/Cadastro";
 import "./App.css";
-
+import Markdown from  "./components/Markdown";
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-      </Routes>
-    </Router>
+  <Markdown/>
   );
 };
 
---
+++ front/src/components/Markdown.tsx
@@ -0,0 +1,8 @@
+const Markdown = ({ content }: { content: string }) => {
+    return (
+        <h1>Testa</h1>
+    );
+    }
+
+
+export default Markdown;
