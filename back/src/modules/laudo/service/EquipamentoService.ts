14e747908f6ff505ea1d6b4e30e413594fa35a96
Guixaadrez1 <guilhermehenrique.a.dasilva12@gmail.com>
2025-06-12 13:26:04 -0300

    Criando service Equipamento

--- back/src/modules/laudo/service/EquipamentoService.ts
+++ back/src/modules/laudo/service/EquipamentoService.ts
@@ -7,6 +7,8 @@ export class EquipamentoClass {
     this.EquipamentoPersistence = new equipamentoPersistence();
   }
 
+  // método para criação do equipamento
+
   async criarEquipamento({
     equipserie,
     equipmodel,
@@ -31,20 +33,61 @@ export class EquipamentoClass {
     idtipoeq: number;
   }) {
     try {
-      const equipamentoNovo = await this.EquipamentoPersistence.create({
-        equipamentoserie: equipserie,
-        equipamentomodelo: equipmodel,
-        equipamentomac: equipmac,
-        equipamentoipv4: equipipv4,
-        equipamentoipv6: equipipv6,
-        equipamentoanydesk: equipanydesk,
-        equipamentodw: equipdwservice,
-        equipamentoalugado: equipalugado,
-        idsede: idsede,
-        idtipoeq: idtipoeq,
-      });
 
+      // Realizando Validações 
+
+      if(!idsede){
+
+        return `É obrigatório a existência de uma sede!`
+      
+      }else if (!idtipoeq){
+
+        return `É obrigatório a existência de um tipo de equipamento`
+      }
+      else if (equipserie && equipserie.length > 30 ) {
+
+        return `A quantidade de caracteres do número de série não pode ser maior que 30`
+      }
+      else if (equipmodel && equipmodel.length > 50){
+
+        return `A quantidade de caracteres do modelo do equipamento não pode ser maior que 50`
+      }
+      else if ( equipipv4 && equipipv4.length > 15){
+        return `É a quantidade de caracteres do IPV4 não pode ser maior que 15 caracteres`
+      }
+      else if (equipipv6 && equipipv6.length > 32){
+
+        return `É a quantidade de caracteres do IPV6 não pode ser maior que 32 caracteres`
+      }
+      else if ( equipmac && equipmac.length > 12){
+
+        return `A quantidade de caracteres do endereço MAC não pode ser maior que 12`
+      }
+      else if (equipanydesk && equipanydesk.length > 10 ){
+
+        return `A quantidade de caracteres do número Anydesk não pode ser maio que 10`
+      }
+      else if (equipdwservice && equipdwservice.length > 10 ){
+
+        return `A quantidade de caracteres do Dw Service não pode ser maior que 10`
+      }
+
+      
+      const equipamentoNovo = await this.EquipamentoPersistence.create({
+        idsede, // id da sede
+        idtipoeq, // id do tipo de equipamento
+        equipamentoserie: equipserie, // serie do equipamento
+        equipamentomodelo: equipmodel, // modelo do equipamento
+        equipamentomac: equipmac, // endereço físico do equipamento MAC
+        equipamentoipv4: equipipv4, // endereço ethernet ipv4 
+        equipamentoipv6: equipipv6, // endereço ethernet ipv6
+        equipamentoanydesk: equipanydesk, // numero do anydesk para acesso remoto
+        equipamentodw: equipdwservice, // nome do agente Dw para acesso remoto
+        equipamentoalugado: equipalugado // se o equipamento está alugado ou não
+      });
+      
       return equipamentoNovo;
+  
     } catch (e) {
       console.error(`Erro ao criar um equipamento: ${e}`);
       throw new Error("Falha na criação do equipamento.");
