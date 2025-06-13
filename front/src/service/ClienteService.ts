bac35c53563b069c627e2ffcbf8b37a62cfccc68
Guixaadrez1 <guilhermehenrique.a.dasilva12@gmail.com>
2025-06-13 15:13:50 -0300

    Usando o package-json da main

--- front/src/service/ClienteService.ts
+++
@@ -1,6 +0,0 @@
-import api from './client-api';
-
-export const listarClientes = async () => {
-  const response = await api.get('/cliente');
-  return response.data;
-};
--- front/src/services/Cliente-Api.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:3000', // CONEXÃO COM O BACK-END
-  withCredentials: true,
-});
-
-export default api;
-
