ee73aa29accb977f9b999a94b953b41c7e27b20b
Gui.Ti <160532307+GuiXaadrez1@users.noreply.github.com>
2025-06-13 15:11:10 +0000

    Refatorando pasta back-end

--- back/src/modules/router/app.ts
+++ back/src/modules/router/app.ts
@@ -1,61 +1,42 @@
+// Importando express e cors
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 
-// Rotas laudinho-2
+// Importando Rotas
 import userRoutes from '../login/routes/usuario-routes';
-import protegidoRoutes from '../login/routes/auth-middleware-routes';
+import router from '../laudo/routes/RouteCliente';
+import enderecoRouter from '../laudo/routes/RouteEndereco';
 import sedeRoutes from '../laudo/routes/sede-routes';
+import protegidoRoutes from '../login/routes/auth-middleware-routes';
 
-// Rotas laudinho-3
-import router from "../laudo/routes/RouteCliente";
-import enderecoRouter from "../laudo/routes/RouteEndereco";
-import sedeRouter from "../laudo/routes/RouteSede";
-
+// Importando dotenv para configuração de variáveis ambiente
 import dotenv from 'dotenv';
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-// importando o dotenv para pegar o arquivo de configuração .env
-import dotenv from 'dotenv';
-dotenv.config();
-
-/* materializando um objeto do nosso Servidor express */
+/* Materializando um objeto do nosso Servidor express */
 const app = express();
-const cors = require('cors');
 
-// laudinho-2
-
+/* Middleware para aceitar requisições JSON e habilitar CORS */
 app.use(express.json());
 app.use(cors());
-app.use('/auth', userRoutes);
-app.use('/api', protegidoRoutes);
-app.use('/sede', sedeRoutes);
 
-app.use(express.json()); // configuramos o nosso express para aceitar requisições json
+/* Configurando as rotas */
+app.use('/auth', userRoutes);           // Usuários
+app.use('/api', protegidoRoutes);       // rotas api protegidas
+app.use('/cliente', router);            // Cliente
+app.use('/sede', sedeRoutes);           // Sede
+app.use('/endereco', enderecoRouter);   // Endereço
 
-/* Configurando express para usar as rotas criadas!  */
-app.use('/cliente', router); // Cliente 
-app.use('/sede', sedeRouter); // Sede
-app.use('/endereco', enderecoRouter); // endereco
-
-app.use(express.json()); // configuramos o nosso express para aceitar requisições json
-
-// express recebendo requisições get no nosso diretório raiz
+/* Rota raiz para teste */
 app.get('/', (req: Request, res: Response) => {
-    const resposta = res.status(200).json({ messagem: "Diretório Raiz" });
-    console.log(resposta);
+    res.status(200).json({ mensagem: "Diretório Raiz" });
 });
 
-
-// Puxando o Port do arquivo.env
-
-const port = 3000
-
-/* Criando o nosso Servidor express */
-app.listen(port, (): void => {
-    const mensagem: string = 'SERVIDOR RODANDO NA PORTA:';
-    console.log(`${mensagem} ${port}`);
+/* Criando o nosso servidor express */
+app.listen(PORT, (): void => {
+    console.log(`SERVIDOR RODANDO NA PORTA: ${PORT}`);
 });
 
 export default app;
