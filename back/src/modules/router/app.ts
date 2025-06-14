6632e01e035cfc4b71104394682904716c7b3f90
Ezio <67691964+EzioPP@users.noreply.github.com>
2025-06-14 20:14:22 -0300

    Conserte estrutura do projeto

--- back/src/modules/router/app.ts
+++ back/src/modules/router/app.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from 'express';
-import cors from 'cors';
 // Rotas laudinho-2
 import userRoutes from '../login/routes/usuario-routes';
 import protegidoRoutes from '../login/routes/auth-middleware-routes';
@@ -17,12 +16,10 @@ const PORT = process.env.PORT || 3000;
 
 /* materializando um objeto do nosso Servidor express */
 const app = express();
-const cors = require('cors');
 
 // laudinho-2
 
 app.use(express.json());
-app.use(cors());
 app.use('/auth', userRoutes);
 app.use('/api', protegidoRoutes);
 app.use('/sede', sedeRoutes);
