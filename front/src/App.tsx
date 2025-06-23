b8513873adbeadbb1aae82a5179ad1b4dc5c0a6c
Guixaadrez1 <guilhermehenrique.a.dasilva12@gmail.com>
2025-06-23 10:05:00 -0300

    Criando uma Rota para tela EQUIPAMENTOS

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,6 +3,7 @@
 //import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import ClientesPage from './pages/ClientesPage';
+import Equipamentos from './pages/Equipamentos';
 import CadastroPage from './pages/cadastro'; // com "C" maiúsculo (match com o export)
 import LoginPage from './pages/login'; // caso exista
 
@@ -15,6 +16,7 @@ function App() {
       <Route path="/cadastro" element={<CadastroPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/clientes" element={<ClientesPage />} />
+      <Route path="/equipamentos" element={<Equipamentos />} />
     </Routes>
   );
 }
